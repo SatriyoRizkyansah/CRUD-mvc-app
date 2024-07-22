ab3da0ab77913a44f8c38d44b8e6cbc34422b88e
satriyo <satttrioo@gmail.com>
2024-07-23 00:46:45 +0700

    solve some errors

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -3,6 +3,7 @@
 class Mahasiswa extends Controller{
     public function index(){
         $data['judul'] = 'Daftar Mahasiswa';
+        $data['title'] = 'Daftar Mahasiswa';
         $data['mhs'] = $this->model('Mahasiswa_model')->getAllMahasiswa();
         $this->view('templates/header', $data);
         $this->view('mahasiswa/index', $data);
